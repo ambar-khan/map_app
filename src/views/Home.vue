1f83701370f797c5e8efa1027d4345af2fa1a90b
Ambar <ambarmogg@me.com>
2020-12-22 17:35:32 -0800

    setup mapbox

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>  
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style>
+body {
+  margin: 0;
+  padding: 0;
 }
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 500px;
+  height: 300px;
+}
+</style>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  mounted: function () {
+    // console.log(process.env.VUE_APP_MY_API_KEY);
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 10, // starting zoom
+    });
+  },
+  methods: {},
+};
 </script>
