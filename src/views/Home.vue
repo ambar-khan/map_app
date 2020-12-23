a94ba902b9e839511e0fa1d19acf14944717dee6
Ambar <ambarmogg@me.com>
2020-12-22 18:32:05 -0800

    attach a popup to a marker instance

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,18 +20,34 @@ body {
 </style>
 
 <script>
+/* global mapboxgl */
 export default {
   name: "Home",
   components: {},
   mounted: function () {
     // console.log(process.env.VUE_APP_MY_API_KEY);
     mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    var monument = [-77.0353, 38.8895];
     var map = new mapboxgl.Map({
       container: "map", // container id
       style: "mapbox://styles/mapbox/streets-v11", // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 10, // starting zoom
+      center: monument, // starting position [lng, lat]
+      zoom: 15, // starting zoom
     });
+    // create the popup
+    var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+      "Construction on the Washington Monument began in 1848."
+    );
+
+    // create DOM element for the marker
+    var el = document.createElement("div");
+    el.id = "marker";
+
+    // create the marker
+    new mapboxgl.Marker(el)
+      .setLngLat(monument)
+      .setPopup(popup) // sets a popup on this marker
+      .addTo(map);
   },
   methods: {},
 };
